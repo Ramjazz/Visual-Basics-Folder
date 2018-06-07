ffe7eb297e01d8d64f4643d8b4b547adc80d083f
Ramjazz <33108223+Ramjazz@users.noreply.github.com>
2018-06-07 12:50:41 -0400

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,27 +1,41 @@
-﻿'Arjun Hariharan, Period 6, addMoney
+﻿'Arjun Hariharan, Period 6, averageScore
 Public Class Form1
 
-    Private Sub addCoins(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnEnter.Click
-        Dim quarters, dimes, nickels, pennies As Single
-        quarters = Val(Me.txtQuarters.Text)
-        dimes = Val(Me.txtDimes.Text)
-        nickels = Val(Me.txtNickels.Text)
-        pennies = Val(Me.txtPennies.Text)
+    Private Sub averageTheScore(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnAverage.Click
+        Dim userResponse As String
+        Dim score As Integer
+        Dim totalScore As Integer
+        Const FLAG As Integer = -1
+        Dim numbScores As Integer
+        Dim average As Single
 
-        Me.lblResponse.Text = FormatCurrency(calculateMoney(quarters, dimes, nickels, pennies))
+        
+
+        Do
+            userResponse = InputBox("Enter score, enter -1 when done", "Average Score")
+            score = Val(userResponse)
+            If score <> FLAG And userResponse <> Nothing Then
+
+                totalScore += score
+                numbScores += 1
+                average = totalScore / numbScores
+            End If
+        Loop While score <> FLAG And userResponse <> Nothing
+
+        If score <> FLAG And userResponse <> Nothing Then
+            Me.lblScoresEntered.Text = "You entered " & numbScores & " scores"
+            Me.lblResponse.Text = average
+
+        Else
+            Me.lblScoresEntered.Text = "You entered " & numbScores & " scores"
+            Me.lblResponse.Text = average
+        End If
+
+        
+        
 
     End Sub
 
-    Private Function calculateMoney(ByVal q As Single, ByVal d As Single, ByVal n As Single, ByVal p As Single) As Single
-        q *= 0.25
-        d *= 0.1
-        n *= 0.05
-        p *= 0.01
 
-        Return q + d + n + p
-    End Function
 
-    Private Sub changeText(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtDimes.TextChanged, txtNickels.TextChanged, txtPennies.TextChanged, txtQuarters.TextChanged
-        Me.lblResponse.Text = ""
-    End Sub
 End Class
